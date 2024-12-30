1eaaac8e5c1a5655ce4959f2a3bd8e1840b406fc
Link Strifer <1057706+linkstrifer@users.noreply.github.com>
2024-12-29 21:27:17 -0500

    refactor: update MultiForm component to use loader for user info validation and change form method to GET

--- app/routes/form.tsx
+++ app/routes/form.tsx
@@ -1,10 +1,13 @@
-import { ActionFunctionArgs } from '@remix-run/node';
-import { Form, useActionData } from '@remix-run/react';
 import Input from '~/components/input';
 import Steps from '~/components/steps';
+import { LoaderFunctionArgs } from '@remix-run/node';
+import { Form, useLoaderData } from '@remix-run/react';
 
-function validateUserInfo(formData: FormData) {
-  const data = Object.fromEntries(formData);
+function validateUserInfo(formData: FormData | Record<string, string>) {
+  const data =
+    typeof formData === 'object'
+      ? { ...formData }
+      : Object.fromEntries(formData);
 
   const errors: {
     name?: string;
@@ -12,85 +15,90 @@ function validateUserInfo(formData: FormData) {
     phone?: string;
   } = {};
 
-  if (!data.name) {
+  console.log({ data });
+
+  if ('name' in data && !data.name) {
     errors['name'] = 'Name is required';
   }
 
-  if (!data.email) {
+  if ('email' in data && !data.email) {
     errors['email'] = 'Email is required';
   }
 
-  if (!data.phone) {
+  if ('phone' in data && !data.phone) {
     errors['phone'] = 'Phone is required';
   }
 
-  if (data.phone.toString().length < 10) {
+  if (data.phone && data.phone.toString().length < 10) {
     errors['phone'] = 'Please enter a valid phone number';
   }
 
   return { errors };
 }
 
-export async function action({ request }: ActionFunctionArgs) {
-  const formData = await request.formData();
+export async function loader({ request }: LoaderFunctionArgs) {
+  const search = new URLSearchParams(request.url.split('?')[1]);
 
-  return validateUserInfo(formData);
+  return validateUserInfo(Object.fromEntries(search.entries()));
 }
 
+type CurrentStep = 'userInfo' | 'plan' | 'addons' | 'summary';
 
 export default function MultiForm() {
-  const actionData = useActionData<typeof action>();
+  const loaderData = useLoaderData<typeof loader>();
+
+  const currentStep: CurrentStep = 'userInfo';
 
   return (
     <main className="bg-[#eef5ff] grid min-h-screen">
       <section className="bg-[#ffffff] flex flex-col md:grid grid-cols-3 md:m-auto md:w-fit gap-4 p-4 rounded-3xl">
         <Steps />
 
-        <article className="flex flex-col justify-center col-span-2 gap-7 text-[#0d284f]">
-          <h1 className="text-3xl">
-            <b>Personal info</b>
-          </h1>
+        {currentStep === 'userInfo' ? (
+          <article className="flex flex-col justify-center col-span-2 gap-7 text-[#0d284f]">
+            <h1 className="text-3xl">
+              <b>Personal info</b>
+            </h1>
 
-          <p className="text-[#bcbdc2]">
-            Please provide your name, email address, and phone number
-          </p>
+            <p className="text-[#bcbdc2]">
+              Please provide your name, email address, and phone number
+            </p>
 
-          <Form className="flex flex-col gap-4" method="POST">
-            <Input
-              error={actionData?.errors.name}
-              placeholder="Name"
-              label="Name"
-              type="text"
-              name="name"
-            />
+            <Form className="flex flex-col gap-4" method="GET">
+              <Input
+                error={loaderData?.errors?.name}
+                placeholder="Name"
+                label="Name"
+                type="text"
+                name="name"
+              />
 
-            <Input
-              error={actionData?.errors.email}
-              placeholder="Email"
-              label="Email"
-              type="email"
-              name="email"
-            />
+              <Input
+                error={loaderData?.errors?.email}
+                placeholder="Email"
+                label="Email"
+                type="email"
+                name="email"
+              />
 
-            <Input
-              error={actionData?.errors.phone}
-              placeholder="Phone number"
-              label="Phone number"
-              type="text"
-              name="phone"
-            />
+              <Input
+                error={loaderData?.errors?.phone}
+                placeholder="Phone number"
+                label="Phone number"
+                type="text"
+                name="phone"
+              />
 
-            <button
-              className="bg-[#174a8b] self-end text-white rounded-md p-2"
-              type="submit"
-            >
-              Next Step
-            </button>
-          </Form>
-        </article>
+              <button
+                className="bg-[#174a8b] self-end text-white rounded-md p-2"
+                type="submit"
+              >
+                Next Step
+              </button>
+            </Form>
+          </article>
+        ) : null}
       </section>
     </main>
   );
 }
-
-
