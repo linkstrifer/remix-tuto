cd7c87a356467942f1aeb7200ffc68fdc5fb9b32
Link Strifer <1057706+linkstrifer@users.noreply.github.com>
2025-01-07 15:21:13 -0500

    refactor form validation logic; implement validateUserInfo utility and add tests

--- app/routes/form.tsx
+++ app/routes/form.tsx
@@ -9,44 +9,7 @@ import {
   calculateCurrentStep,
   CurrentStep,
 } from '~/utils/calculateCurrentStep/calculateCurrentStep';
-
-function validateUserInfo(
-  formData: FormData | Record<string, string | string[]>
-) {
-  console.log({ formData });
-  const data =
-    typeof formData === 'object'
-      ? { ...formData }
-      : Object.fromEntries(formData);
-
-  const errors: Record<string, string> = {};
-
-  const validData = Object.getOwnPropertyNames(data);
-
-  if ('name' in data && !data.name) {
-    errors['name'] = 'Name is required';
-  }
-
-  if ('email' in data && !data.email) {
-    errors['email'] = 'Email is required';
-  }
-
-  if ('phone' in data && !data.phone) {
-    errors['phone'] = 'Phone is required';
-  }
-
-  if (data.phone && data.phone.toString().length < 10) {
-    errors['phone'] = 'Please enter a valid phone number';
-  }
-
-  return {
-    errors,
-    validData: validData.filter(
-      (inputName) => !Object.getOwnPropertyNames(errors).includes(inputName)
-    ),
-    formData: data,
-  };
-}
+import { validateUserInfo } from '~/utils/validateUserInfo/validateUserInfo';
 
 export async function loader({ request }: LoaderFunctionArgs) {
   const search = new URLSearchParams(request.url.split('?')[1]);
